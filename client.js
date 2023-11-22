88f1ba56d672b917512f92a6d7b0b8f259ad4865
jszady <jszady99@gmail.com>
2023-11-22 15:36:30 -0500

    making snake move up only up

--- client.js
+++ client.js
@@ -1,18 +1,33 @@
 const net = require('net');
 
-const connect = function() {
+const connect = function() 
+{
   // when we return conn we will return this object that connects to the server
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
-  })
-  conn.setEncoding('utf-8');
+  });
+  // we are writing a message to know that we connected to the server
+  conn.on("connect", () => {
+ 
+// this would make us move up but only up
+  // setInterval(() => {
+  //   conn.write("Move: up");
+  // }, 100);
+
+ 
+  console.log("WE IN THE SERVER ");
+  
+  conn.write("Name: Jan");
+  });
+  // right when we connect we are sending data over to the server to move our snake up
   // will output what server is saying to the client
   conn.on("data", (data) =>{
     console.log(data);
-  })
-
-
+  });
+  
+  
+  conn.setEncoding('utf-8');
   return conn;
 }
 module.exports =  {
